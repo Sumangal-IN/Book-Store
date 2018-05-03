6589abc7ee3e429391c00d36352b212bdb97f018
mandal_s <sumangal.mandal@kingfisher.com>
2018-05-03 22:34:50 +0100

    First Commit

---
+++ Book Store/src/main/java/com/kingfisher/bookStore/book/Book.java
@@ -0,0 +1,47 @@
+package com.kingfisher.bookStore.book;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "data")
+	private String data;
+
+	public Book() {
+
+	}
+
+	public Book(int id, String data) {
+		super();
+		this.id = id;
+		this.data = data;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
---
+++ Book Store/src/main/java/com/kingfisher/bookStore/book/BookRepository.java
@@ -0,0 +1,11 @@
+package com.kingfisher.bookStore.book;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+	List<Book> findById(int id);
+}
